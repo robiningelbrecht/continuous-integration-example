8846138f287cae79eb38791748e73d9a7cc95283
robin <robin@baldwin.be>
2022-03-19 15:03:55 +0100

    A failing PR

---
+++ src/Domain/Pizza/Topping/Pineapple.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Domain\Pizza\Topping;
+
+use Money\Money;
+
+class Pineapple extends BaseTopping
+{
+    protected function giveMeThePrice(): Money
+    {
+        // TODO: Implement giveMeThePrice() method.
+    }
+
+    protected function giveMeTheDescription(): string
+    {
+        // TODO: Implement giveMeTheDescription() method.
+    }
+}
